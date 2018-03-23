6013c445b61f1c1026f028bbe1e0b88cc1a04598
William Wong <compulim@hotmail.com>
2018-03-22 23:22:08 -0700

    Add comment

--- redux/loadStore.js
+++ redux/loadStore.js
@@ -24,6 +24,7 @@ module.exports = function loadStore(session) {
   );
 
   production || store.subscribe(() => {
+    // TODO: Save store
     session.send({
       type: 'event',
       name: 'store',
