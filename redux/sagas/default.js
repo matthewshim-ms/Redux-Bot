42421f25338bd4975bb8b84115ddce30e576f34b
William Wong <compulim@hotmail.com>
2018-04-06 02:46:04 -0700

    Cleanup

--- redux/sagas/default.js
+++ redux/sagas/default.js
@@ -17,7 +17,6 @@ module.exports = function* (session) {
       yield put(sendMessage(`Welcome to the Search City bot. I\'m currently configured to search for things in ${ city }`));
       yield put(promptText('Before get started, please tell me your name?'));
     } else if (!username) {
-
       yield put(setUsername(text));
       yield put(sendMessage(`Welcome ${ text }!\n * If you want to know which city I'm using for my searches type 'current city'. \n * Want to change the current city? Type 'change city to cityName'. \n * Want to change it just for your searches? Type 'change my city to cityName'`));
     } else if (changeCityMatch) {
